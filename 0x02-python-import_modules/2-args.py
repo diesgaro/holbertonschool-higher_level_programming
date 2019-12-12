acab9277fa781768fef381b06f3ce0d7fcd37f62
Diego Garzon <digarodev@gmail.com>
2019-12-12 19:34:20 +0000

    fix act. 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     if argl == 1:
         print("{:d} {:s}.".format(0, "arguments"))
     else:
-        print("{:d} {:s}:".format(argl - 1
-                                  , "arguments" if argl > 2 else "argument"))
+        print("{:d} {:s}:"
+              .format(argl - 1, "arguments" if argl > 2 else "argument"))
         for i in range(argl):
             if i > 0:
                 print("{:d}: {:s}".format(i, argv[i]))
