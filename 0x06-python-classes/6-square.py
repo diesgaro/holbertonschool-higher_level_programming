e13cc23a0fb99b2c5473093491ba122c39da76fe
Diego Garzon <digarodev@gmail.com>
2020-01-18 17:25:31 +0000

    Act. 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ __init_ Constructor """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or \
+           len(value) != 2 or \
+           type(value[0]) != int or type(value[1]) != int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    """ Area """
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size is 0:
+            print("")
+            return
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
