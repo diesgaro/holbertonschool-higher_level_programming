4254c1e2806e69e53a52f9d39c530eb4769bfc54
Diego Garzon <digarodev@gmail.com>
2020-01-21 22:00:40 +0000

    act. 0 1 2 3

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, 'r', encoding='utf-8') as f:
+        list = f.readlines()
+
+    if nb_lines <= 0 or nb_lines >= len(list):
+        for line in list:
+            print (line, end='')
+    else:
+        for i in range(nb_lines):
+            print (list[i], end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+#0x0B-python-input_output
+
+This directory has the activities for the project 0x0B python input output
