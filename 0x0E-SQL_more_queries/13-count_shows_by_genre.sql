8b18e7cb8ccb40601008fac318c50bd98347f3c0
Diego Garzon <digarodev@gmail.com>
2020-03-03 19:00:15 +0000

    Fix sentence as by AS

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Query that list all genres and displays the number of shows linked
 -- to each using inner join and group by
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) as number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
