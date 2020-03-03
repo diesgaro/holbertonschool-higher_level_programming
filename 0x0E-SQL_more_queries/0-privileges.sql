77c347313fad4030206cfe9562c1ef6dabb93709
Diego Garzon <digarodev@gmail.com>
2020-03-03 18:06:47 +0000

    add flush privileges sentence

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Query thath list all privileges of some users
+-- Query that list all privileges of some users
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
