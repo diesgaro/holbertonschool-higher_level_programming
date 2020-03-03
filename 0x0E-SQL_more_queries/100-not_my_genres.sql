f1b2a20747d45c71226c5debe58ee5a3e5dc521a
Diego Garzon <digarodev@gmail.com>
2020-03-03 18:48:35 -0500

    fix task 17, change from to FROM

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,2 +1,2 @@
 -- Query that list the genres not linked by the show dexter using sub query
-SELECT gen.name from tv_genres gen WHERE gen.id NOT IN (SELECT sg.genre_id FROM tv_show_genres sg INNER JOIN tv_shows tv ON sg.show_id = tv.id WHERE tv.title = "Dexter") ORDER BY gen.name ASC;
+SELECT gen.name FROM tv_genres gen WHERE gen.id NOT IN (SELECT sg.genre_id FROM tv_show_genres sg INNER JOIN tv_shows tv ON sg.show_id = tv.id WHERE tv.title = "Dexter") ORDER BY gen.name ASC;
