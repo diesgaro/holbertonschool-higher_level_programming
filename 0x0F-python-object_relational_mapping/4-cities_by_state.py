aff8033b0e828e2d7abd5a5635dea0bca876d63e
Diego Garzon <digarodev@gmail.com>
2020-03-27 18:09:13 -0500

    task 4, list all cities with his state using inner join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Python script that get and show row filtered by name passing
+how argument avoid sql injecction
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT city.id, city.name, state.name" \
+            " FROM cities AS city" \
+            " INNER JOIN states AS state ON city.state_id = state.id" \
+            " ORDER BY city.id ASC;"
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
