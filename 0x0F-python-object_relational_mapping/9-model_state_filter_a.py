991bb5fa2c3db09f3e0ecd33100aad185913d059
Diego Garzon <digarodev@gmail.com>
2020-03-29 17:08:12 -0500

    select all rows that contain a letter in the name of table states

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Script that select all rows that contain 'a' letter
+    from states table using SQLAlchemy
+"""
+
+import sys
+import json
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(
+        State.name.like('%a%')).order_by(
+            (State.id))
+
+    for row in query.all():
+        print("{}: {}".format(row.id, row.name))
