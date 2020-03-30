d3230ae48ab9b9765a68b22f6ca85c71270e3f04
Diego Garzon <digarodev@gmail.com>
2020-03-29 23:40:48 -0500

    fix query using BINARY for compare a value with exact case sensitive match

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
                          db=argv[3])
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(
-        argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC;".\
+        format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
