747849a9aedadc3cc9e1ac49928fd744cf41f0b2
Diego Garzon <digarodev@gmail.com>
2020-03-29 21:37:58 -0500

    remove rows that contain --a letter

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Script that delete rows that contain 'a' letter
+    from states table using SQLAlchemy
+"""
+
+import sys
+import json
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.name.like('%a%')).\
+        delete(synchronize_session=False)
+
+    session.commit()
