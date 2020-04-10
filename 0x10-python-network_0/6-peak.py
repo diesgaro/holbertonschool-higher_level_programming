998cd35d88059e39211cc093ee390967c843e652
Diego Garzon <digarodev@gmail.com>
2020-04-09 19:09:42 -0500

    task 6, I using Max function, but I dont believe it is than easy

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Peak Module """
+
+
+def find_peak(list_of_integers):
+    """
+    find_peak function
+    Args:
+    list_of_integers
+    """
+    if list_of_integers:
+        return max(list_of_integers)
