34a9f21f21ed88114838b28ba1ce020e353404be
Diego Garzon <digarodev@gmail.com>
2020-04-15 19:34:37 -0500

    change all for the holberton checker

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ from urllib.request import urlopen
 url = argv[1]
 
 with urlopen(url) as response:
-    val_header = response.getheader('X-Request-Id')
+    val_header = response.getheader('X-Request-ID')
 
 print(val_header)
