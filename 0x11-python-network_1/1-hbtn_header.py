57c9d13e49976f71b3d42811ab7e7f3a03858b56
Diego Garzon <digarodev@gmail.com>
2020-04-15 19:01:04 -0500

    get response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+from sys import argv
+from urllib.request import urlopen
+
+my_url = argv[1]
+
+with urlopen(my_url) as response:
+    print(response.headers['X-Request-Id'])
