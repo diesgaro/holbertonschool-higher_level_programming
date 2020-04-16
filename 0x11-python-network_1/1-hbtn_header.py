b18964dbbee9649447c51c5925ecdf22491ecc16
Diego Garzon <digarodev@gmail.com>
2020-04-15 19:40:06 -0500

    the holberton checker is crazy

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@ Script that get response header X-Request-Id
 import urllib.request
 from sys import argv
 
-with urllib.request.urlopen(argv[1]) as response:
-    val_header = response.getheader('X-Request-ID')
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        val_header = response.getheader('X-Request-ID')
 
-print(val_header)
+    print(val_header)
