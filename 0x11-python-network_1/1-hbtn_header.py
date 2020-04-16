d098f27909c8bcfccb23a96c3d3c312d80051f27
Diego Garzon <digarodev@gmail.com>
2020-04-15 19:43:07 -0500

    return all, was missing the __name__ : __main__

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ Script that fetches https://intranet.hbtn.io/status
 from sys import argv
 from urllib.request import urlopen
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     my_url = argv[1]
 
     with urlopen(my_url) as response:
