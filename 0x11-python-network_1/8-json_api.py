5fdc3678b0453a301e8d4b2527700a5995c93994
Diego Garzon <digarodev@gmail.com>
2020-04-19 19:09:03 -0500

    script that sends post request and handle the json returned

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that sends post request and the service return a Json
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = 'http://6ab867a77f21.19.hbtn-cod.io:5000/search_user'
+    parameter = ""
+    send_parameters = {}
+
+    if (len(argv) > 1):
+        parameter = argv[1]
+
+    send_parameters['q'] = parameter
+
+    req = requests.post(url, data=send_parameters)
+
+    try:
+        print('No result') if req.json() == {} else print(
+            '[{}] {}'.
+            format(
+                req.json()['id'],
+                req.json()['name']
+            )
+        )
+    except ValueError as e:
+        print('Not a valid JSON')
