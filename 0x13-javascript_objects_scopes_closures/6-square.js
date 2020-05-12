c2c49205859a719880144f24f3643f33debc5640
Diego Garzon <digarodev@gmail.com>
2020-05-12 13:39:54 -0500

    define the method charPrint

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+Define Class Square extends Rectangle
+*/
+
+const rectangle = require('./4-rectangle');
+
+class Square extends rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
