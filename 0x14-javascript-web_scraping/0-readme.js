f6176185ff4abab39f19155212e4210c30ebb3db
Diego Garzon <digarodev@gmail.com>
2020-05-12 22:32:02 -0500

    change the conditions of the process file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
+/*
+    Read and print content file
+*/
 const fs = require('fs');
 const args = process.argv;
 
 fs.readFile(args[2], 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+  if (data) {
+    console.log(data);
+  } else {
+    console.log(err);
   }
-  console.log(data);
 });
