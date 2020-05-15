d29d4ca75588164a40cd6d5f5a5e57dd92774143
Diego Garzon <digarodev@gmail.com>
2020-05-15 15:08:02 -0500

    remove some options for the request

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,12 +6,7 @@ const request = require('request');
 const args = process.argv;
 
 const options = {
-  url: args[2],
-  method: 'GET',
-  headers: {
-    'Accept-Charset': 'utf-8',
-    'User-Agent': 'Holberton-1265'
-  }
+  url: args[2]
 };
 
 request(options, (err, res, body) => {
