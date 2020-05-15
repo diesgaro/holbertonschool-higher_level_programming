2aee904c3d91cf37b46ca85d9969a9106144a22a
Diego Garzon <digarodev@gmail.com>
2020-05-15 14:45:48 -0500

    make request and search specific pattern

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/*
+    Make a request and print the result
+*/
+const request = require('request');
+const args = process.argv;
+
+const options = {
+  url: args[2],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'Holberton-1265'
+  }
+};
+
+request(options, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const obj = JSON.parse(body);
+    let count = 0;
+    for (let i = 0; i < obj.count; i++) {
+      for (let j = 0; j < obj.results[i].characters.length; j++) {
+        const strFind = obj.results[i].characters[j].search('/people/18/');
+        if (strFind !== -1) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
