7b699e75f2be5dadb108f6708643b3ff98211981
Diego Garzon <digarodev@gmail.com>
2020-05-15 15:28:18 -0500

    fix the count of results

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,33 @@
 #!/usr/bin/node
 /*
-  Star wars Wedge Antilles
+    Make a request and print the result
 */
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let cnt = 0;
-    JSON.parse(body).results.forEach(character => {
-      character.characters.forEach(link => {
-        if (link.endsWith('/18/')) {
-          cnt++;
-        }
-      });
-    });
-    console.log(cnt);
+const args = process.argv;
+
+const options = {
+  url: args[2],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'Holberton-1265'
   }
+};
+
+request(options, (err, res, body) => {
+  let count = 0;
+  if (err) {
+    console.log(err);
+  } else {
+    const obj = JSON.parse(body);
+    for (let i = 0; i < obj.results.length; i++) {
+      for (let j = 0; j < obj.results[i].characters.length; j++) {
+        const strFind = obj.results[i].characters[j].search('/people/18/');
+        if (strFind !== -1) {
+          count++;
+        }
+      }
+    }
+  }
+  console.log(count);
 });
